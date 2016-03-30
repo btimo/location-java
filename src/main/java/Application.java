05899f3974f080d1144547a2d5cab9dc71947642
btimo <timo.b35@gmail.com>
2016-03-30 22:37:33 +0800

    - mise en place du logger - deplacement de Application.java - suppression de LocAppli (doublon par rapport au role de Application) - debut mise en place mode 'prod' et 'dev'

---
+++ src/main/java/Application.java
@@ -0,0 +1,83 @@
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.EbeanServerFactory;
+import com.avaje.ebean.config.ServerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import views.InterfaceLocation;
+
+/**
+ * Classe utilisée pour le lancement des fenêtres
+ */
+public class Application {
+
+    /**
+     * Change this variable if in production mode
+     * This will stop Ebean from deleting and re-creating the DB
+     */
+    private static final boolean isDeveloppement = true;
+
+    private static EbeanServer server;
+
+    protected static Logger logger = LoggerFactory.getLogger(Application.class);
+
+    private static Application app = null;
+
+    public static void main(String[] args){
+        Application app = getApp();
+
+        ServerConfig c = new ServerConfig();
+        c.setName("sqlite");
+
+        // read the ebean.properties and load
+        // those settings into this serverConfig object
+        c.loadFromProperties();
+
+        // generate DDL and run it
+        c.setDdlGenerate(isDeveloppement);
+        c.setDdlRun(isDeveloppement);
+
+        // register as the 'Default' server
+        c.setDefaultServer(true);
+
+        server = EbeanServerFactory.create(c);
+    }
+
+    /**
+     * Constructeur privé
+     */
+    private Application(){
+        System.out.println("Application Location");
+
+        // initialize data of the application
+        // initData();
+    }
+
+    /**
+     * Obtention du singleton
+     * @return singleton App
+     */
+    public static Application getApp()
+    {
+        if(app == null){
+            app = new Application();
+        }
+
+        return app;
+    }
+
+    /**
+     * Chargement de l'interfae Swing
+     */
+    public void load() {
+        InterfaceLocation.load();
+    }
+
+    /**
+     * Ajout des données aux programmes
+     * Utilisé en phase de développement seulement
+     */
+    public void initData(){
+        //
+    }
+}
--- src/test/java/EmprunteursTest.java
+++ src/test/java/EmprunteursTest.java
@@ -13,13 +13,9 @@ public class EmprunteursTest {
     @Test
     public void getLengthshouldReturn3(){
         Adresse adresse = new Adresse(3, "avenue de la République", "94800", "Villejuif");
-        //System.out.println(adresse);
         Adresse adresse2 = new Adresse(3, "avenue des champs Elysées", "75800", "Paris");
         Adresse adresse3 = new Adresse(3, "place de la Comédie", "34000", "Montpellier");
 
-        /**
-         * Test d'emprunteur
-         */
         Emprunteur emprunteur = new Emprunteur(adresse, "Adrien", "Poupa");
         emprunteur.save();
         Emprunteur emprunteur2 = new Emprunteur(adresse2, "Stéphane", "Gateau");
@@ -28,10 +24,6 @@ public class EmprunteursTest {
         emprunteur3.save();
 
         Emprunteurs.fetchAll();
-        System.out.println(Emprunteurs.get().size());
-        for(Emprunteur e: Emprunteurs.get()){
-            System.out.println(e);
-        }
         assertEquals("Il devrait y avoir 3 emprunteur", 3, Emprunteurs.get().size());
     }
 }
